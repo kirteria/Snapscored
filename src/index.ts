3f221b468ee4fb787de5cea40ae4ad2fd8c582e8
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2026-05-08 19:17:27 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import homeApp from "../api/index.js";
 import snapscored from "../api/snapscored.js";
 import snapunks from "../api/snapunks.js";
+import snapunks from "../api/gmfarcaster.js";
 
 const app = new Hono();
 
 app.route("/", homeApp);
 app.route("/snapscored", snapscored);
 app.route("/snapunks", snapunks);
+app.route("/gmfarcaster", gmfarcaster);
 
 export default app;
